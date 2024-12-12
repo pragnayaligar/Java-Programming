81299bd6c4cdc9fc8b3cea9196176e0cbdcc1e3a
pragnayaligar <pragnayaligar@gmail.com>
2024-12-12 22:56:40 +0530

    Add new codes

---
+++ Mul.java
@@ -0,0 +1,13 @@
+package proj2;
+import java.util.Scanner;
+public class Mul {
+
+	public static void main(String[] args) {
+		int number = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+
+	}
+
+}
