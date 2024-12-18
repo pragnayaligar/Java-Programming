45bb33a78880829e8584c2f1fb4ec753ad92d2e9
pragnayaligar <pragnayaligar@gmail.com>
2024-12-18 23:38:47 +0530

    Added codes

---
+++ Ascii.java
@@ -0,0 +1,15 @@
+package proj2;
+
+public class Ascii {
+
+	public static void main(String[] args) {
+		char ch = 'a';
+        int ascii = ch;
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    
+	}
+
+}
