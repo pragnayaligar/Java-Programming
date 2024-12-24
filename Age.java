be84b45fc06a25c6f497e396a85190fc9d2b7aca
pragnayaligar <pragnayaligar@gmail.com>
2024-12-24 13:48:02 +0530

    Update Age.java

--- Age.java
+++ Age.java
@@ -8,7 +8,7 @@ public class Age {
 		String name=sc.nextLine();
 		System.out.println("Enter your age: ");
 		int age=sc.nextInt();
-		System.out.println("Hello my name is " + name + " and I am " + age +" years old");
+		System.out.println("Hello my name is " + name + " and I am " + age );
 		
 	}
 
